37cad8b8a5d882a70c672bd3206a4a30ee820b2c
mamun <mamun.rashid@seequent.com>
2021-04-30 05:46:13 -0600

    set an image at center

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,11 @@ void main() {
     MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text("I am Rich")),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
       ),
     ),
   );
